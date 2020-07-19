513ff81befc3459ac0f36ca19de64a8ad60ba4a6
bahtiyar720 <bahtiyar720@gmail.com>
2020-07-19 00:44:47 -0700

    add two

--- src/test/java/com/blueSprint/stepDefinitions/LoginStepDfs.java
+++ src/test/java/com/blueSprint/stepDefinitions/LoginStepDfs.java
@@ -2,18 +2,21 @@ package com.blueSprint.stepDefinitions;
 
 import com.blueSprint.pages.LoginPage;
 import com.blueSprint.utilities.Driver;
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 
+import java.util.List;
+
 public class LoginStepDfs {
     LoginPage loginPage = new LoginPage();
-
-    @When("User login as valid {string} and {string}")
-    public void userLoginAsValidAnd(String userName, String password) {
-        loginPage.userInput.sendKeys( userName );
-        loginPage.passwordInput.sendKeys( password );
+    @When("User login as valid username and password")
+    public void userLoginAsValidUsernameAndPassword(DataTable credentials) {
+        List<List<String>>data=credentials.asLists();
+        loginPage.userInput.sendKeys( data.get( 0 ).get( 0 ) );
+        loginPage.passwordInput.sendKeys( data.get( 0 ).get( 1 ) );
         loginPage.loginButton.click();
     }
 
@@ -24,5 +27,6 @@ public class LoginStepDfs {
         String expectedTitle = "(26) Portal";
         Assert.assertEquals( actualTitle,expectedTitle );
     }
+
 
 }
--- src/test/resources/features/Login.feature
+++ src/test/resources/features/Login.feature
@@ -1,7 +1,10 @@
 Feature: Login
   Agile story: as user i should use valid credential i should able
   to successfully login and see the dashboard.
+
   @smoke @failTest
   Scenario:Login scenario
-    When User login as valid "helpdesk29@cybertekschool.com" and "UserUser"
+    When User login as valid username and password
+      | helpdesk29@cybertekschool.com | UserUser |
+
     Then User should able to see dashboard
